ec4ec42e4f04eedc49e7ef78e50c7679bd18df78
Markus Rößler <markus.roessler@obi.de>
2022-06-15 16:55:08 +0200

    initial commit

---
+++ UnexpectedAppearingOrder/MyPage1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UnexpectedAppearingOrder
+{
+    public partial class MyPage1 : ContentPage
+    {
+        public MyPage1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Console.WriteLine("OnAppearing: {0}", this);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Console.WriteLine("OnDisappearing: {0}", this);
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Console.WriteLine(" --- Navigating ---");
+            Navigation.PushAsync(new MyPage2());
+        }
+    }
+}
+
---
+++ UnexpectedAppearingOrder/MyPage2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UnexpectedAppearingOrder
+{
+    public partial class MyPage2 : ContentPage
+    {
+        public MyPage2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Console.WriteLine("OnAppearing: {0}", this);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Console.WriteLine("OnDisappearing: {0}", this);
+        }
+    }
+}
+
